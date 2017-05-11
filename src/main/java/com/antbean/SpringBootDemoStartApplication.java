ec40eb9b4570cb6395de0f1e8e35cdb6275b51ff
lengyucc <1902328305@qq.com>
2017-05-11 22:59:55 +0800

    init

---
+++ src/main/java/com/antbean/SpringBootDemoStartApplication.java
@@ -0,0 +1,12 @@
+package com.antbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoStartApplication.class, args);
+	}
+}
