445e4100f4e5a3d5bb10a74d5fc91254c7588e69
lengyucc <1902328305@qq.com>
2017-05-12 11:04:26 +0800

    test and devtools

--- src/test/java/com/antbean/SpringBootDemoStartApplicationTests.java
+++ src/test/java/com/antbean/SpringBootDemoStartApplicationTests.java
@@ -1,16 +1,39 @@
 package com.antbean;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.antbean.controller.HelloWorldController;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SpringBootDemoStartApplication.class)
+@SpringApplicationConfiguration(classes = MockServletContext.class)
+@WebAppConfiguration
 public class SpringBootDemoStartApplicationTests {
 
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() {
+		mvc = MockMvcBuilders.standaloneSetup(new HelloWorldController()).build();
+	}
+
 	@Test
-	public void contextLoads() {
+	public void testHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON)) //
+				.andExpect(MockMvcResultMatchers.status().isOk()) //
+				.andDo(MockMvcResultHandlers.print()) //
+				.andReturn();
 	}
 
 }
